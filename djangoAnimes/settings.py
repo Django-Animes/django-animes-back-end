31b4a1702cecbf7fc4abefef4bdbb16eea6394e0
Artorys <darksoulsgui123@gmail.com>
2023-01-02 16:00:04 -0500

    feat: hidden the secret key with dotenv

--- djangoAnimes/settings.py
+++ djangoAnimes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import  os
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-maz-q!)2w_$tj)9#((m#r*s_7t)1$u4t^dao^*(u_q9xss5$s6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
