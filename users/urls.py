4dc13eebceaaf8d37638cf6c82a25d2019ac2d2e
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-01-04 16:35:17 -0300

    feat: serializer fix and views

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt import views as jwt_views
+
+urlpatterns = [
+    path("users/", views.UserView.as_view()),
+    path("users/<int:pk>/", views.UserDetailView.as_view()),
+    path("users/login/", jwt_views.TokenObtainPairView.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from .models import User
+from .serializers import UserSerializer
+from .permissions import IsAccountOwner
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class UserView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class userDetailView(RetrieveUpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
