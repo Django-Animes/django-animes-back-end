6cd6c1cda3829d11e4bfbad828f659b12781a55a
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2023-01-04 20:11:10 -0300

    fix: relation of achievements moved to user

--- achievements/models.py
+++ achievements/models.py
@@ -8,8 +8,8 @@ class Achievement(models.Model):
         name = models.CharField(max_length=255)
         url = models.CharField(max_length=255)
         type_of_achievement = models.CharField(max_length=255)
-        profiles = models.ManyToManyField(
+        users = models.ManyToManyField(
             User,
-            through="achievement_profile",
+            through="achievement_user",
             related_name="achievements",
         )
