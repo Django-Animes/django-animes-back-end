950eca689dd36fa95f409e00e6de62085646d639
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-05 18:10:45 -0300

    feat: Views comment

--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,14 @@
 from .models import Comment
+from .permissions import IsCommentOwner
 from .serializers import CommentSerializer
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class CommentView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
     queryset = Comment.objects.all()
@@ -13,9 +16,8 @@ class CommentView(CreateAPIView):
 
 
 class CommentDetailView(RetrieveUpdateDestroyAPIView):
-    permission_classes = []
+    permission_classes = [IsCommentOwner]
     authentication_classes = [JWTAuthentication]
-    
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
