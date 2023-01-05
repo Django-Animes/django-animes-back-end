dc0d036ba0fa9a509f4920bfe56fc6b65b3f1475
byetevinn <stevan14padilha@gmail.com>
2023-01-05 16:47:19 -0300

    feat: Urls Profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import ProfileSerializer
+from .models import Profile
 
-# Create your views here.
+
+class ProfileListCreate(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    # authentication - JWT
+
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class ProfileUpdateDelete(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    # authentication - JWT
+
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
