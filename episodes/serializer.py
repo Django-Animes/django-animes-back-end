c1922b3350574f792d6d3dc327cc5b166166cb28
Artorys <darksoulsgui123@gmail.com>
2023-01-04 21:35:05 -0500

    feat: add episode route

--- episodes/serializer.py
+++ episodes/serializer.py
@@ -4,6 +4,11 @@ from animes.models import Anime
 class AnimeNameSerializer(serializers.Serializer):
     name = serializers.CharField()
 
+class AnimeSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Anime
+        fields = ["id","name","image_url","release_date","description"]
+        extra_kwargs = {"episodes" : {"write_only" : True},"genres" : {"write_only" : True}}
 
 class AnimeEpisodeSerializer(serializers.ModelSerializer):
     class Meta():
@@ -17,8 +22,18 @@ class EpisodeSerializer(serializers.ModelSerializer):
     class Meta():
         model = Episode
         fields = "__all__"
-    anime = AnimeNameSerializer()
+    anime = AnimeSerializer()
 
     def create(self, validated_data):
         episode = Episode.objects.get_or_create(**validated_data)
         return episode
+
+    def update(self, instance, validated_data : dict):
+        anime = validated_data.pop("anime",None)
+        if anime:
+            anime,_ = Anime.objects.get_or_create(**anime)
+        for key,value in validated_data.items():
+            setattr(instance,key,value)
+        instance.anime = anime
+        return instance
+            
