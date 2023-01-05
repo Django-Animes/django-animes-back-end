04f55906eee25f25081f1b15fddf0c94996a3f1c
Artorys <darksoulsgui123@gmail.com>
2023-01-05 08:13:22 -0500

    fix: auth user model const

--- djangoAnimes/settings.py
+++ djangoAnimes/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-MY_APPS = ["animes", "genres", "episodes", "comments"]
+MY_APPS = ["animes", "genres", "episodes", "comments","users"]
 
 THIRD_PARTY_APPS = ["rest_framework", "rest_framework_simplejwt"]
 
@@ -130,3 +130,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
