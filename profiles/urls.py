4a5ed0a51f37c1c83024cdf28c56607c7371d5b8
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-05 20:43:30 -0300

    feat: Urls comment

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import ProfileListCreate, ProfileUpdateDelete
+from comments.views import CommentCreateView, CommentDetailView
 
 urlpatterns = [
     path("user/profile/", ProfileListCreate.as_view()),
     path("user/profile/", ProfileUpdateDelete.as_view()),
+    path("comment/<int:pk>/", CommentDetailView.as_view()),
+    path("comment/episode/<int:pk>/", CommentCreateView.as_view()),
+    path("comment/", CommentDetailView.as_view()),
 ]
