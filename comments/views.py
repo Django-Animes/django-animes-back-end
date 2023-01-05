e541244ce23b8ed95c33112b9b81a07e6f748f67
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-05 11:56:51 -0300

    feat: Comment view

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class CommentView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = []
+    authentication_classes = [JWTAuthentication]
+    
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
