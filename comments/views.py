b84851e500d1c3286193ec09ef0bb21f2b97dae5
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-05 22:10:58 -0300

    feat: Perform create of comments views

--- comments/views.py
+++ comments/views.py
@@ -3,6 +3,10 @@ from .permissions import IsCommentOwner
 from .serializers import CommentSerializer
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from episodes.models import Episode
+from profiles.models import Profile
+from rest_framework.views import Response
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -13,6 +17,13 @@ class CommentCreateView(CreateAPIView):
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        profile_id = self.request.data["profile"]
+        profile_id = profile_id["id"]
+        profile = get_object_or_404(Profile, id=profile_id)
+        episode = get_object_or_404(Episode, id=self.kwargs["pk"])
+        return serializer.save(profile=profile, episode=episode)
 
 
 class CommentDetailView(RetrieveUpdateDestroyAPIView):
