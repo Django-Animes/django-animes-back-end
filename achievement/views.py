070f853ec43b885c4b070f82e3556758f5167099
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2023-01-06 20:34:11 -0300

    fix: trade arquiv names

--- achievement/views.py
+++ achievement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Achievement
-from .serializer import AchievementSerializer
+from .serializers import AchievementSerializer
 from rest_framework.generics import ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
