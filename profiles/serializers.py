7c782285d6988376440007499927af43439fe7f8
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-05 22:12:00 -0300

    fix: Debuging models and serializer of profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,7 +3,9 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    last_episode_viewed = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
         fields = "__all__"
-        read_only_fields = ["id", "created_at"]
+        read_only_fields = ["id", "created_at", "last_episode_viewed", "user"]
