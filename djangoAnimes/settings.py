0acf2194fe94e60cc1e72c6317aceaf1ecf05d51
Artorys <darksoulsgui123@gmail.com>
2023-02-02 21:38:57 -0500

    fix

--- djangoAnimes/settings.py
+++ djangoAnimes/settings.py
@@ -36,10 +36,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
 # Application definition
 MY_APPS = [
     "animes",
